637ed96c1d27ca20f38ad4204fdaf49cdc770286
Paul Cooke <paulgfcooke@gmail.com>
2019-12-16 10:22:42 +0000

    database connected and logging connected message

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 8000
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/experimentation1'
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  },
+  () => console.log('Mongo is connected')
+)
 
 app.listen(PORT, () => console.log(`Up and running on port ${PORT}`))
+
+
